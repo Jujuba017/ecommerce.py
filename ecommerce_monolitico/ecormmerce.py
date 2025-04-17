860a898055f4974a4fa81a8db2ead71f05b9f839
JULIA APARECIDA SANTOS RODRIGUES <00001087562053sp@aluno.educacao.sp.gov.br>
2025-04-17 09:18:36 -0300

    primeiro commit

---
+++ ecommerce_monolitico/ecormmerce.py
@@ -0,0 +1,25 @@
+class EcommerceSystem:
+
+    def __init__(self):
+       self.users = []
+       self.products = []
+       self.orders = []
+
+    def add_user(self, user):
+       self.users.append(user)
+
+    def add_product(self, product):
+       self.products.append(product)
+
+
+    def place_order(self, user, product):
+        if product in self.products:
+            self.orders.append((user, product))
+        return "Order placed successfully"
+        return "Product not available"
+
+
+system = EcommerceSystem()
+system.add_user("julia ap")
+system.add_product("Laptop")
+print(system.place_order("julia ap", "Laptop"))
